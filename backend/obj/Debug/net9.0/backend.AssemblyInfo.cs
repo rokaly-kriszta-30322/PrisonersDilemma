bbfbdb8346a6f30f80cd8544feb2394b66d6ed27
Kriszta Rokaly <rokalykriszta@gmail.com>
2025-12-15 02:04:02 +0200

    provider

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966b2b964ee489bc427ff1010021352eb3e39ef1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1c435af8c371cb3a4a468495bf1acccc6eafdf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
